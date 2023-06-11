c66c827ae079337f6913837b6c7310a48e473883
Fatih Yavuz <yvzfth@yandex.com>
2023-06-11 16:07:43 +0300

    first commit

--- KennyApp/ViewController.swift
+++ KennyApp/ViewController.swift
@@ -8,12 +8,129 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let gamePlaySeconds = 10
+    var randomX = 0.0
+    var randomY = 0.0
+    var countdownTimer: Timer?
+    var kennyTimer: Timer?
+    var remainingSeconds = 0
+    var score = 0
+    var highscore = 0
+    
+    @IBOutlet var parentView: UIView!
+    @IBOutlet var highscoreLabel: UILabel!
+    var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    var imageViewHalfWidth = 0.0
+    var imageViewHalfHeight = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageViewHalfWidth = Double(imageView.frame.width/2)
+        imageViewHalfHeight = Double(imageView.frame.height/2)
+        timeLabel.text = String(gamePlaySeconds)
+        
+        if let storedHighScore = UserDefaults.standard.object(forKey: "highscore") {
+            highscore = storedHighScore as! Int
+            highscoreLabel.text = "Highscore: \(storedHighScore)"
+        }
+        
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        imageView.isUserInteractionEnabled = true
+       
     }
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        // Perform actions when the image view is tapped
+        UIImageView.animate(withDuration: 0.4, animations: {
+            self.imageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                               self.imageView.alpha = 0
+           }) { (_) in
+               DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                   self.imageView.transform = CGAffineTransform.identity
+                   self.imageView.alpha = 1
+                   self.imageView.isHidden = false
+               }
+           }
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+        
+    }
+    
+    @IBAction func startCountdown() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func moveKenny() {
+        
+        kennyTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateKennyPositions), userInfo: nil, repeats: true)
+    }
+    @objc func updateKennyPositions() {
+        if remainingSeconds > 0 {
+            randomX = Double.random(in: imageViewHalfWidth...Double(parentView.frame.width - imageViewHalfWidth))
+            randomY = Double.random(in: imageViewHalfHeight...Double(parentView.frame.height - imageViewHalfHeight))
+            imageView.layer.position.x = randomX
+            imageView.layer.position.y = randomY
+           // imageView.isHidden = false
+        }
+        else {
+            //imageView.isHidden = false
+            kennyTimer?.invalidate()
+            
+        }
+    }
+    
+    @objc func updateTimer() {
+        if remainingSeconds > 0 {
+            remainingSeconds -= 1
+            // Update countdown label or perform other actions
+            timeLabel.text = String(remainingSeconds)
+        } else {
+            imageView.removeGestureRecognizer(tapGestureRecognizer)
+            countdownTimer?.invalidate() // Stop the timer
+            // Countdown has reached zero, perform any desired actions
+            if(score > highscore) {
+            highscore = score
+                highscoreLabel.text = "Highscore: \(highscore)"
+            }
+            UserDefaults.standard.set(highscore, forKey: "highscore")
+            let alert = UIAlertController(title: "Time's Up", message: "Would you like to replay?", preferredStyle: .alert)
 
+                   let okAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                       // OK button action
+                       self.startButton.isHidden = false
+                   }
+                   alert.addAction(okAction)
 
+                   let replyAction = UIAlertAction(title: "Replay", style: .default) { _ in
+                       // Reply button action
+                       self.play()
+                       
+                   }
+                   alert.addAction(replyAction)
+
+                   present(alert, animated: true, completion: nil)
+        }
+    }
+    @IBOutlet var startButton: UIButton!
+    @IBAction func startClicked(_ sender: Any) {
+     play()
+    }
+    
+    func play(){
+        score = 0
+        scoreLabel.text = "Score: \(score)"
+        timeLabel.text = String(gamePlaySeconds)
+        remainingSeconds = gamePlaySeconds
+        startCountdown()
+        moveKenny()
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        imageView.isUserInteractionEnabled = true
+        startButton.isHidden = true
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,54 @@
+# KennyApp
+
+KennyApp is a fun iOS game where the objective is to tap on a randomly moving Kenny image to score points. You have 10 seconds to tap as many Kenny images as you can. Challenge yourself to beat your high score!
+
+## Features
+
+- Randomly moving Kenny image.
+- Tap on the Kenny image to score points.
+- 10 seconds to play.
+- High score saved on the user's device.
+
+## Requirements
+
+- iOS 16.4+
+- Xcode 14.3.1+
+- Swift 5.0+
+
+## Installation
+
+1. Clone the repository or download the source code.
+2. Open the project in Xcode.
+3. Build and run the app on a simulator or physical device.
+
+## Usage
+
+1. Launch the Kenny app on your iOS device.
+2. The game will start automatically.
+3. Tap on the Kenny image as many times as you can within 10 seconds.
+4. Each tap on the Kenny image will increase your score by 1.
+5. Try to beat your high score with each play.
+
+## Persistence
+
+The app saves the high score on the user's device using UserDefaults. The high score will persist even if the app is closed or the device is restarted.
+
+## Contributing
+
+Contributions to the Kenny app are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+The Kenny app is released under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+The Kenny app is inspired by the classic "Whack-a-Mole" game.
+
+## Screenshots
+
+Include screenshots of the app in action to provide visual context and entice users to try it out.
+
+---
+
+Feel free to customize this README file based on your specific app features, requirements, and preferences.
